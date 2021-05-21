87600e71fcce77f66396401a47f4b752c629771d
huginn <huginn@huginn.me>
2021-05-21 07:43:37 +0200

    Created message objects for main menu vista manager

--- Assets/Scripts/Core/UI/UI View/Messages/Current View Set/CurrentViewSetMessage.cs
+++ Assets/Scripts/Core/UI/UI View/Messages/Current View Set/CurrentViewSetMessage.cs
@@ -1,5 +1,8 @@
 namespace HoodedCrow.Core.UI.Messages
 {
+    using UnityEngine;
+
+    [CreateAssetMenu(menuName = "Example/UI/Messages/Current View Set")]
     public class CurrentViewSetMessage: AMessage<CurrentViewSetMessageContent>
     {
 
