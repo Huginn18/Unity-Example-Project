23a80ddd78f7c4c8617fdb1b3887fa91adb94397
huginn <huginn@huginn.me>
2021-05-21 06:35:23 +0200

    implemented message system in scene loader

---
+++ Assets/Scripts/Core/Scene Loader/Messages/Scene Unloaded/SceneUnloadedMessage.cs
@@ -0,0 +1,9 @@
+namespace HoodedCrow.Core
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(menuName = "Example/Core/Scene Loader/Scene Unloaded Message")]
+    public class SceneUnloadedMessage: AMessage<SceneUnloadedMessageContent>
+    {
+    }
+}
--- Assets/Scripts/Core/Scene Loader/SceneLoader.cs
+++ Assets/Scripts/Core/Scene Loader/SceneLoader.cs
@@ -2,25 +2,43 @@ namespace HoodedCrow.Core
 {
     using UnityEngine;
     using UnityEngine.AddressableAssets;
+    using UnityEngine.ResourceManagement.AsyncOperations;
+    using UnityEngine.ResourceManagement.ResourceProviders;
     using UnityEngine.SceneManagement;
 
     public class SceneLoader: MonoBehaviour
     {
+        [SerializeField] private LoadSceneMessage _loadSceneMessage;
+        [SerializeField] private SceneLoadedMessage _sceneLoadedMessage;
+        [SerializeField] private UnloadSceneMessage _unloadSceneMessage;
+        [SerializeField] private SceneUnloadedMessage _sceneUnloadedMessage;
+
+
         private void Awake()
         {
-            //ToDo: List to loadSceneMessage
+            _loadSceneMessage.AddListener(HandleLoadSceneMessage);
+            _unloadSceneMessage.AddListener(HandleUnloadSceneMessage);
+        }
+
+        private void HandleLoadSceneMessage(LoadSceneMessageContent content)
+        {
+            LoadAdditiveScene(content.SceneReference);
+        }
+        private void HandleUnloadSceneMessage(UnloadSceneMessageContent content)
+        {
+            UnloadScene(content.SceneReference);
         }
 
         private void LoadAdditiveScene(AssetReference sceneReference)
         {
-            sceneReference.LoadSceneAsync(LoadSceneMode.Additive);
-            //ToDo: Add scene loaded message
+            AsyncOperationHandle<SceneInstance> asyncOperationHandle = sceneReference.LoadSceneAsync(LoadSceneMode.Additive);
+            asyncOperationHandle.Completed += handle => _sceneLoadedMessage.Send(new SceneLoadedMessageContent(handle.Result));
         }
 
         private void UnloadScene(AssetReference sceneReference)
         {
-            sceneReference.UnLoadScene();
-            //ToDo: Add scene unloaded message
+            var asyncOperationHandle = sceneReference.UnLoadScene();
+            asyncOperationHandle.Completed += handle => _sceneUnloadedMessage.Send(new SceneUnloadedMessageContent(handle.Result));
         }
     }
 }
