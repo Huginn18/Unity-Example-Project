3b241452c37c54655ff89c7c7e85354571d375b5
huginn <huginn@huginn.me>
2021-05-21 20:38:39 +0200

    implemented start quiz view logic

---
+++ Assets/Scripts/UI/Views/Quiz/StartQuizView.cs
@@ -0,0 +1,46 @@
+namespace HoodedCrow.UI
+{
+    using HoodedCrow.Core;
+    using HoodedCrow.Core.UI;
+    using UnityEngine;
+    using UnityEngine.AddressableAssets;
+    using UnityEngine.UI;
+
+    public class StartQuizView: AView
+    {
+
+        [SerializeField] private Button _yesButton;
+        [SerializeField] private Button _noButton;
+
+        [Space, SerializeField] private AssetReference _mainMenuScene;
+
+        [Header("Messages - Sends")]
+        [SerializeField] private LoadSceneMessage _loadSceneMessage;
+
+        public override void Initialize(IViewController<AView> viewController)
+        {
+            base.Initialize(viewController);
+
+            _yesButton.onClick.AddListener(OnYesButton);
+            _noButton.onClick.AddListener(OnNoButton);
+        }
+
+        protected override void OnShowView()
+        {
+        }
+
+        protected override void OnHideView()
+        {
+        }
+
+        private void OnYesButton()
+        {
+            _viewController.ShowView<QuizQuestionView>(false);
+        }
+
+        private void OnNoButton()
+        {
+            _loadSceneMessage.Send(new LoadSceneMessageContent(_mainMenuScene));
+        }
+    }
+}
